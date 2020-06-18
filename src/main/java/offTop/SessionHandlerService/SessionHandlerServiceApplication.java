a9ff6263794a3e632cbe3054a3eaa001dab848b4
marcello <marcello.sanagustin.448@my.csun.edu>
2020-06-18 01:35:48 -0700

    added spring files

---
+++ src/main/java/offTop/SessionHandlerService/SessionHandlerServiceApplication.java
@@ -0,0 +1,13 @@
+package offTop.SessionHandlerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionHandlerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionHandlerServiceApplication.class, args);
+	}
+
+}
